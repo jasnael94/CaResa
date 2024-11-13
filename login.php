54459194e1114720446ecd10d1da05e29ed7945c
Denisdcb <denis.dcbr@gmail.com>
2024-11-13 11:52:23 +0100

    Update login page

--- login.php
+++ login.php
@@ -1,22 +1,20 @@
 <?php
-// session_start();
-// $pdo = new PDO('mysql:host=localhost;dbname=', 'root', '');
+    session_start();
 
-// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//     $email = $_POST['email'];
-//     $password = $_POST['password'];
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-//     $stmt = $pdo->prepare("SELECT * FROM Client WHERE email = :email");
-//     $stmt->execute([':email' => $email]);
-//     $user = $stmt->fetch();
-
-//     if ($user && password_verify($password, $user['password_hash'])) {
-//         $_SESSION['user_id'] = $user['id'];
-//         header("Location: home.php");
-//     } else {
-//         echo "<p>Identifiants invalides.</p>";
-//     }
-// }
+        if(!empty($username) && !empty($password)) {
+            if(($username === "Dechambre" || $username === "Raj") && $password === "Royalride2024") {
+                $connection = "Connection réussi, redirection vers la page d'accueil.." ;
+                $_SESSION['login'] = $username ;
+                header('refresh: 2 url="home.php"');
+            }
+        } else {
+            echo "Erreur veuillez remplir les champs..";
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -24,15 +22,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href=" "> -->
+    <link rel="stylesheet" href="./style.css">
     <title>RoyalRide-Connexion</title>
 </head>
 <body>
-    <h1>RoyalRide</h1>
-    <form method="POST">
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Mot de passe" required>
-        <button type="submit">Se connecter</button>
-    </form>
+    <div class="login-bg">
+        <div class="login-content">
+            <h1>Royal Ride</h1>
+            <h2>Login</h2>
+            <form action="login.php" method="post" class="login-form">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="Enter your username" required>
+                <label for="passord">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter your password" required>
+                <button type="submit">Connect</button>
+            </form>
+            <p><?php echo $connection ?></p>
+        </div>
+    </div>
 </body>
 </html>
