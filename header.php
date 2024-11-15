78f7409c1d55e37f4a4c8cab7ed2da6c8cc8eec8
Jason <jasnael94@gmail.com>
2024-11-15 09:50:14 +0100

    addinf favicon

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>RoyalRide</title>
   <link rel="stylesheet" href="/css/style.css">
+  <link rel="icon" type="image/gif" href="/pictures/favicon.png" />
 </head>
 
 <body>
