88bf37e2e85138b456315a21209bc75b4f606520
Jason <jasnael94@gmail.com>
2024-11-20 16:16:45 +0100

    updated home

--- home.php
+++ home.php
@@ -4,34 +4,43 @@
     require_once('header.php');
     echo $css;
 ?>
-<div class="homestyle">
-    <section class=>
+<div>
+    <section class="section1">
+        <div class="div1">
+            <h2>LOCATION DE SUV ou BERLINE </h2>
+            <p class="ph">Nous nous engageons à offrir une expérience de location de voiture sans faille, avec un service client disponible 24h/24 et 7j/7. Découvrez le luxe à son apogée avec RoyalRide et laissez-nous vous conduire vers de nouvelles aventures. Faites de votre prochain trajet un moment inoubliable, à la hauteur de vos attentes.</p> 
+            <button class="discover"><a href="/cars.php">Decouvrir</a></button>   
+        </div>
+    </section>
+</div>
+<div >
+    <section class=section2>
         <div class="homecont">
-            <h3>Présentation</h3>
+            <h3>PRESENTATION</h3>
             <p class="ph">Bienvenue chez RoyalRide, le leader de la location de voitures de luxe. Spécialisés dans l'expérience haut de gamme, nous vous offrons une flotte exclusive de véhicules de prestige, allant des berlines de luxe aux voitures sportives. Chaque modèle a été soigneusement sélectionné pour son confort, ses performances et son design exceptionnel, afin de répondre aux attentes les plus élevées de nos clients.
 
 Chez RoyalRide, nous croyons que chaque voyage doit être une expérience mémorable. Que vous ayez besoin d'une voiture pour un événement spécial, un week-end d'aventure ou un déplacement professionnel, nous mettons à votre disposition des véhicules qui allient élégance et puissance.
 
-Nos services incluent la possibilité de louer des voitures avec chauffeur, un service de livraison et de récupération à votre domicile ou à votre hôtel, ainsi que des options de personnalisation pour répondre à vos besoins spécifiques.
-
-Nous nous engageons à offrir une expérience de location de voiture sans faille, avec un service client disponible 24h/24 et 7j/7. Découvrez le luxe à son apogée avec RoyalRide et laissez-nous vous conduire vers de nouvelles aventures. Faites de votre prochain trajet un moment inoubliable, à la hauteur de vos attentes.</p> 
+Nos services incluent la possibilité de louer des voitures avec chauffeur, un service de livraison et de récupération à votre domicile ou à votre hôtel, ainsi que des options de personnalisation pour répondre à vos besoins spécifiques.</p> 
               
-            </div>
+        </div> 
 
             <div class="homecont">
-                <h3>Qui sommes nous?</h3>
+                <h3>QUI SOMMES NOUS?</h3>
                 <p class="ph">RoyalRide a été fondée sur une simple idée : rendre l'expérience de location de voiture de luxe accessible, sans compromis sur la qualité, le confort ou le service. Notre équipe est composée de passionnés de voitures et de professionnels du secteur de l'automobile, unis par la volonté de transformer chaque trajet en une expérience d'exception.
 
 Nous sommes dédiés à offrir un service client personnalisé, en mettant l'accent sur les détails qui font la différence. Grâce à notre large sélection de véhicules de luxe, vous pouvez choisir la voiture qui correspond parfaitement à votre style et à vos besoins. Que ce soit pour une escapade en ville, un événement important ou une expérience sportive, nous avons la voiture qu'il vous faut.
 
 RoyalRide se distingue également par son engagement envers l'innovation et la satisfaction client. Nous offrons des services supplémentaires, tels que des options d'assurance, des accessoires haut de gamme et des chauffeurs professionnels pour vous garantir un confort optimal.
 
-Notre objectif est de vous offrir plus qu'une simple location : une expérience sur mesure, à la hauteur de vos attentes. Avec RoyalRide, chaque trajet devient un véritable moment de plaisir et de luxe.</p>
+Notre objectif est de vous offrir plus qu'une simple location : une expérience sur mesure, à la hauteur de vos attentes.</p>
             
             </div>
 
     </section>
+    
 </div>
+
 <?php
     require_once('footer.php');
 ?>
