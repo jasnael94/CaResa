01c85c0d79f91a220ac124a6feeb7513e4f96d9b
Calista VOLGET <calistavolget@gmail.com>
2024-11-22 17:00:16 +0100

    readjust css for faq and booking

--- V2/Views/booking/booking.html.php
+++ V2/Views/booking/booking.html.php
@@ -12,19 +12,19 @@
                                 <h3>Réservation n°<?php echo$idResa;?></h3>
                                 <h4>Informations de la réservation :</h4><br>
                                     <ul>
-                                        <li>Nom : <?php echo $_SESSION['login'];?></li>
-                                        <li>Date de réservation : <?php echo $bookingAvailables['createdAt'] ?></li>
-                                        <li>Date de début : <?php echo $bookingAvailables['startDate'] ?></li>
-                                        <li>Date de fin : <?php echo $bookingAvailables['endDate'] ?></li>
-                                        <li>Prix total : <?php echo $bookingAvailables['total'] ?>€</li>                                       
-                                        <li class="status-open">Statut : <?php echo $bookingAvailables['statut'] ?></li>
+                                        <li><strong>Nom : </strong><?php echo $_SESSION['login'];?></li>
+                                        <li><strong>Date de réservation : </strong><?php echo $bookingAvailables['createdAt'] ?></li>
+                                        <li><strong>Date de début : </strong><?php echo $bookingAvailables['startDate'] ?></li>
+                                        <li><strong>Date de fin : </strong><?php echo $bookingAvailables['endDate'] ?></li>
+                                        <li><strong>Prix total : </strong><?php echo $bookingAvailables['total'] ?>€</li>                                       
+                                        <li class="status-open"><strong>Statut : </strong><?php echo $bookingAvailables['statut'] ?></li>
                                     </ul><br>
                                     <h4>Informations sur le véhicule réservé :</h4><br>
                                     <ul>
-                                        <li>Marque : <?php echo $bookingAvailables['brand'];?></li>
-                                        <li>Modèle : <?php echo $bookingAvailables['model'];?></li>
-                                        <li>Année : <?php echo $bookingAvailables['yearProd'];?></li>
-                                        <li>Prix/Jour : <?php echo $bookingAvailables['pricePerDay'];?>€</li>
+                                        <li><strong>Marque : </strong><?php echo $bookingAvailables['brand'];?></li>
+                                        <li><strong>Modèle : </strong><?php echo $bookingAvailables['model'];?></li>
+                                        <li><strong>Année : </strong><?php echo $bookingAvailables['yearProd'];?></li>
+                                        <li><strong>Prix/Jour : </strong><?php echo $bookingAvailables['pricePerDay'];?>€</li>
                                     </ul>
                             </div>
                         <?php
---
+++ V2/config/constant.php
@@ -0,0 +1,6 @@
+<?php
+define('PATH', 'c:\Users\User\Desktop\miniprojet\RoyalRide\V2');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASS', '');
+define('MYSQL_PORT', '3306');
+define('MYSQL_HOST', 'mysql:dbname=royalride;host=127.0.0.1');
