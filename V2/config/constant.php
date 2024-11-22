93df7a761b0de2aaab4d9ae7abeae337483fcfe6
Denisdcb <denis.dcbr@gmail.com>
2024-11-22 15:14:38 +0100

    Delete V2/config/constant.php

--- V2/config/constant.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('PATH', 'C:\Users\denis\OneDrive\Bureau\Donkeyschool\ProjetResa\RoyalRide\V2');
-
-define('MYSQL_USER', 'root');
-define('MYSQL_PASS', '');
-define('MYSQL_PORT', '3306');
-define('MYSQL_HOST', 'mysql:dbname=royalride;host=127.0.0.1');
