e01f9eab30392a1cc1b142c3fa5d8cf0fc12eb32
Calista VOLGET <calistavolget@gmail.com>
2024-11-26 12:32:48 +0100

    css for faq

--- V2/Views/faq/faq.html.php
+++ V2/Views/faq/faq.html.php
@@ -4,38 +4,38 @@
     </div>
     
     <div class="faq-content">
-        <div class="faq-item">
+        <div class="faq-item left">
             <h3>Quels modes de paiement acceptez-vous ?</h3>
             <p class="answer">
-                Nous acceptons les paiements par carte bancaire (Visa, Mastercard, American Express), virement bancaire, chèques, et espèces, selon certaines conditions. Contactez-nous pour plus de détails.
+                Nous acceptons les paiements par carte bancaire (Visa, Mastercard, American Express), virement bancaire, chèques, et espèces, selon certaines conditions. <br> Contactez-nous pour plus de détails.
             </p>
         </div>
 
-        <div class="faq-item">
+        <div class="faq-item right">
             <h3>Quelles sont les conditions pour louer un véhicule de prestige ?</h3>
             <p class="answer">
-                Vous pouvez consulter toutes nos conditions de location en cliquant sur ce <a href="conditions.php" style="color: #1e73be;">lien</a>.
+                Vous pouvez consulter toutes nos conditions de location en cliquant <a href="conditions" target="_blank">ici</a>.
             </p>
         </div>
 
-        <div class="faq-item">
+        <div class="faq-item left">
             <h3>Comment fonctionne la franchise lors de la location ?</h3>
             <p class="answer">
-                La franchise est une somme d'argent prélevée sur la carte du conducteur principal, servant à limiter sa responsabilité en cas de sinistre. Un rachat partiel de la franchise est disponible, moyennant un supplément. Le montant de la franchise est utilisé pour couvrir les frais en cas de dommages.
+                La franchise est une somme d'argent prélevée sur la carte du conducteur principal, servant à limiter sa responsabilité en cas de sinistre. <br>Un rachat partiel de la franchise est disponible, moyennant un supplément. <br> Le montant de la franchise est utilisé pour couvrir les frais en cas de dommages.
             </p>
         </div>
 
-        <div class="faq-item">
+        <div class="faq-item right">
             <h3>Qu'est-ce qu'une caution ?</h3>
             <p class="answer">
-                La caution est le montant que nous bloquons temporairement sur la carte du conducteur principal pour garantir la franchise en cas de sinistre. Elle est libérée une fois le véhicule restitué et vérifié conforme.
+                La caution est le montant que nous bloquons temporairement sur la carte du conducteur principal pour garantir la franchise en cas de sinistre. <br> Elle est libérée une fois le véhicule restitué et vérifié conforme.
             </p>
         </div>
 
-        <div class="faq-item">
+        <div class="faq-item left">
             <h3>Comment est calculé le forfait kilométrique d’un véhicule avec chauffeur ?</h3>
             <p class="answer">
-                Le forfait kilométrique est calculé en fonction du trajet entre notre garage et le point de prise en charge du véhicule, puis du retour à notre dépôt. Selon le véhicule, le départ peut être effectué depuis des points différents.
+                Le forfait kilométrique est calculé en fonction du trajet entre notre garage et le point de prise en charge du véhicule, puis du retour à notre dépôt. <br> Selon le véhicule, le départ peut être effectué depuis des points différents.
             </p>
         </div>
     </div>
